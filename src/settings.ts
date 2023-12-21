f34214bcf89a4b2ac5fbc0524f268b32be2664ca
Donavan Becker <donavan.becker@icloud.com>
2023-12-21 17:37:29 -0600

    Initial commit

---
+++ src/settings.ts
@@ -0,0 +1,20 @@
+import { PlatformConfig } from 'homebridge';
+/**
+ * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
+ */
+export const PLATFORM_NAME = 'CloudflaredTunnel';
+
+/**
+ * This must match the name of your plugin as defined the package.json
+ */
+export const PLUGIN_NAME = 'homebridge-cloudflared-tunnel';
+
+//Config
+export interface CloudflaredTunnelPlatformConfig extends PlatformConfig {
+  url: string;
+  port: number;
+  hostname: string;
+  protocol: string;
+  verifyTLS: boolean;
+  logging?: string;
+}
