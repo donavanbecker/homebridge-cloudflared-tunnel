c0f3d76156b7da2af6928a44b2fa921dfb499af3
Donavan Becker <donavan.becker@icloud.com>
2023-12-21 19:47:24 -0600

    import instead of require

--- src/platform.ts
+++ src/platform.ts
@@ -1,5 +1,4 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const { startTunnel } = require('ctun');
+import { startTunnel } from 'ctun';
 import { CloudflaredTunnelPlatformConfig } from './settings';
 import { API, DynamicPlatformPlugin, Logger, PlatformAccessory, Service, Characteristic } from 'homebridge';
 
